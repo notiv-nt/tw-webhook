40b2e145817a9ed9fd858f38f74d49a5fd5da23c
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-02-14 14:15:13 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,14 +3,9 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
 bot.start((ctx) => {
-  console.dir(ctx);
-  // const chatId = msg.chat.id;
-  // // send back the matched "whatever" to the chat
-  // bot.sendMessage(chatId, `Your webhook url:\n ${process.env.APP_URL}/${chatId}`);
+  const chatId = ctx.chat.id;
+  ctx.reply(`Your webhook url:\n${process.env.APP_URL}/${chatId}`);
 });
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 module.exports = {
   bot,
