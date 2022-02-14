b4eb8f73c6f5e871cb35be5ba25eec6c62d1ff19
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2022-02-14 14:15:10 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,8 +6,15 @@ const { bot } = require('./bot');
 function main() {
   const PORT = process.env.PORT || 5000;
 
-  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
   bot.launch();
+
+  ['SIGINT', 'SIGTERM'].forEach((code) => {
+    process.once(code, () => {
+      bot.stop(code);
+      server.close();
+    });
+  });
 }
 
 main();
