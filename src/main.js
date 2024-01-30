7a82d9b5cf579d40388b2b4c021c664ba61419c5
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2024-01-30 19:54:08 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const fs = require('fs');
+// const fs = require('fs');
 const http = require('http');
-const https = require('https');
+// const https = require('https');
 const express = require('express');
 const { Telegraf } = require('telegraf');
 
@@ -39,25 +39,25 @@ function main() {
   const PORT = parseInt(process.env.PORT, 10) || 5100;
 
   let httpServer = http.createServer(app).listen(PORT, () => console.log(`HTTP Server running on port ${PORT}`));
-  let httpsServer = null;
+  // let httpsServer = null;
 
-  if (process.env.NODE_ENV === 'production') {
-    try {
-      // https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
-      const credentials = {
-        key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/privkey.pem`, 'utf8'),
-        cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/cert.pem`, 'utf8'),
-        ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/chain.pem`, 'utf8'),
-      };
+  // if (process.env.NODE_ENV === 'production') {
+  //   try {
+  //     // https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+  //     const credentials = {
+  //       key: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/privkey.pem`, 'utf8'),
+  //       cert: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/cert.pem`, 'utf8'),
+  //       ca: fs.readFileSync(`/etc/letsencrypt/live/${process.env.APP_DOMAIN}/chain.pem`, 'utf8'),
+  //     };
 
-      httpsServer = https.createServer(credentials, app);
-      httpsServer.listen(443, () => console.log('HTTPS Server running on port 443'));
-    } catch (e) {
-      if (e.code === 'ENOENT') {
-        console.error('No certificate found, skipping HTTPS server');
-      }
-    }
-  }
+  //     httpsServer = https.createServer(credentials, app);
+  //     httpsServer.listen(443, () => console.log('HTTPS Server running on port 443'));
+  //   } catch (e) {
+  //     if (e.code === 'ENOENT') {
+  //       console.error('No certificate found, skipping HTTPS server');
+  //     }
+  //   }
+  // }
 
   bot.launch();
 
@@ -65,7 +65,7 @@ function main() {
     process.once(code, () => {
       bot.stop(code);
       httpServer?.close();
-      httpsServer?.close();
+      // httpsServer?.close();
     });
   });
 }
