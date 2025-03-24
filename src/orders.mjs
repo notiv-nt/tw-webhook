bdbed4ee2f317174170c17c0b46a17d9ca83d11d
Mikhail Novikov <notiv.nt@gmail.com>
2025-03-24 21:54:12 +0300

    Update orders.mjs

--- src/orders.mjs
+++ src/orders.mjs
@@ -1,42 +1,16 @@
 /**
  * {
- *   [auth_key]: { id: Order },
+ *   [auth_key]: any[],
  * }
  */
 
 const items = {};
 
 export default (app) => {
-  app.get('/o/:auth', (req, res) => res.json(Object.values(items[req.params.auth] || {})));
+  app.get('/o/:auth', (req, res) => res.json(items[req.params.auth] || []));
 
   app.post('/o/:auth', (req, res) => {
-    const item = getItem(req.params.auth, req.body.ticket);
-
-    // create new
-    if (!item) {
-      items[req.params.auth][req.body.ticket] = req.body;
-      return res.json(null);
-    }
-
-    // update old
-    if (item._emit_time <= req.body._emit_time) {
-      items[req.params.auth][req.body.ticket] = req.body;
-    }
-
+    items[req.params.auth] = req.body;
     return res.json(null);
   });
-
-  app.delete('/o/:auth/:id', (req, res) => {
-    try {
-      delete items[req.params.auth][req.params.id];
-    } catch (e) {}
-    res.json(null);
-  });
 };
-
-function getItem(auth, id) {
-  if (!items[auth]) {
-    items[auth] = {};
-  }
-  return items[auth][id] || null;
-}
