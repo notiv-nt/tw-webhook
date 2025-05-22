763ebf0d2f67aecbc6a5353a61bd1499947cdce9
Mikhail Novikov <10743009+notiv-nt@users.noreply.github.com>
2025-05-23 00:50:59 +0300

    Update orders.mjs

--- src/orders.mjs
+++ src/orders.mjs
@@ -1,16 +1,17 @@
-/**
- * {
- *   [auth_key]: any[],
- * }
- */
-
-const items = {};
+const items = {
+  // [auth_key]: {
+  //   [source]: any[]
+  // }
+};
 
 export default (app) => {
   app.get('/o/:auth', (req, res) => res.json(items[req.params.auth] || []));
 
-  app.post('/o/:auth', (req, res) => {
-    items[req.params.auth] = req.body;
+  app.post('/o/:auth/:source', (req, res) => {
+    if (!items[req.params.auth]) {
+      items[req.params.auth] = {};
+    }
+    items[req.params.auth][req.params.source] = req.body;
     return res.json(null);
   });
 };
